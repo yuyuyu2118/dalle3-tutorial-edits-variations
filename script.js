bc2ed8fc5da34b654c698e2b48a3bd31f15a29d1
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-11-19 23:58:08 +0900

    edit作成中

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
 const promptInput = document.getElementById('prompt-input');
 const urlInput = document.getElementById('url-input');
+const modeSelect = document.getElementById('mode-select');
 const imageContainer = document.getElementById('image-container');
 const visionContainer = document.getElementById('vision-container');
 
@@ -8,8 +9,10 @@ const visionContainer = document.getElementById('vision-container');
 promptInput.addEventListener('input', () => {
   if (promptInput.value !== '') {
     urlInput.disabled = true;  // URL入力を無効化
+    modeSelect.disabled = true; // モード選択を無効化
   } else {
     urlInput.disabled = false; // URL入力を有効化
+    modeSelect.disabled = false; // モード選択を有効化
   }
 });
 
@@ -17,8 +20,21 @@ promptInput.addEventListener('input', () => {
 urlInput.addEventListener('input', () => {
   if (urlInput.value !== '') {
     promptInput.disabled = true;  // プロンプト入力を無効化
+    modeSelect.disabled = true; // モード選択を無効化
   } else {
     promptInput.disabled = false; // プロンプト入力を有効化
+    modeSelect.disabled = false; // モード選択を有効化
+  }
+});
+
+// モード選択のイベントリスナー
+modeSelect.addEventListener('change', () => {
+  if (modeSelect.value === '') {
+    promptInput.disabled = false; // プロンプト入力を有効化
+    urlInput.disabled = false;  // URL入力を無効化
+  } else {
+    promptInput.disabled = true;  // プロンプト入力を無効化
+    urlInput.disabled = true; // URL入力を有効化
   }
 });
 
@@ -27,21 +43,28 @@ form.addEventListener('submit', (event) => {
 
   const promptValue = promptInput.value;
   const urlValue = urlInput.value;
+  const modeValue = modeSelect.value;
 
   // 入力によって、関数を変更
   if (promptValue) {
     generateImage(promptValue);
   } else if (urlValue) {
     generateVision(urlValue);
+  } else if (modeValue === 'edits') {
+    generateEdits();
+  } else if (modeValue === 'variations') {
+    generateVariations();
   }
 
   // 入力フィールドを再度有効化
   promptInput.disabled = false;
   urlInput.disabled = false;
+  modeSelect.disabled = false;
 
   // 入力フィールドをクリア
   promptInput.value = '';
   urlInput.value = '';
+  modeSelect.value = '';
 });
 
 async function generateImage(prompt) {
@@ -93,5 +116,30 @@ async function generateVision(url) {
 
   } catch (error) {
     console.error('説明文の生成に失敗しました。', error);
+  }
+}
+
+async function generateEdits() {
+  try {
+    const response = await fetch('http://localhost:3000/generateEdits', {
+      method: 'POST',
+    });
+
+    if (!response.ok) {
+      throw new Error(`Server responded with error: ${errorText}`);
+    }
+
+    const data = await response;
+    console.log(data)
+    if (data) {
+      const imageElement = document.createElement('img');
+      imageElement.src = data.image;
+      imageContainer.appendChild(imageElement);
+    } else {
+      throw new Error('画像URLがレスポンスに含まれていません。');
+    }
+
+  } catch (error) {
+    console.error('画像の生成に失敗しました。', error);
   }
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const OpenAI = require('openai');
 const cors = require('cors');
 const express = require('express');
+const fs = require('fs');
+const sharp = require('sharp');
 
 // デフォルトでOpenAI APIキーは環境変数から自動取得されます
 const openai = new OpenAI();
@@ -58,6 +60,70 @@ app.post('/generateVision', async (req, res) => {
   } catch (error) {
     res.status(500).send({
       error: '説明文の生成に失敗しました。',
+      details: error.message
+    });
+  }
+});
+
+app.post('/generateEdits', async (req, res) => {
+  try {
+    console.log('Request received for image edit.');
+
+    // // 画像をリサイズする
+    // console.log('Resizing image.');
+    // const resizedImageBuffer = await sharp('public/santa1.png')
+    //   .resize(500) // 幅を500ピクセルに設定。高さは自動調整。
+    //   .toBuffer();
+    // console.log('Image resized.');
+
+    // // リサイズした画像をRGBカラースペースに変換し、PNGフォーマットで保存
+    // console.log('Processing resized image with Sharp.');
+    // const processedImage = await sharp(resizedImageBuffer)
+    //   .toColorspace('srgb') // RGBカラースペースに変換
+    //   .toFormat('png') // PNGフォーマットに変換
+    //   .withMetadata() // 元のメタデータを保持
+    //   .toBuffer();
+    // console.log('Processed image ready to send.');
+
+    // 元の画像のサイズを取得
+    const originalImage = 'public/image_edit_original.png';
+    const originalMetadata = await sharp(originalImage).metadata();
+
+    // マスクを元の画像と同じサイズにリサイズ
+    const maskImage = 'public/image_edit_mask.png';
+    await sharp(maskImage)
+      .resize(originalMetadata.width, originalMetadata.height)
+      .toFile('public/resized_mask.png');
+
+    const maskPath = "public/santa1_mask.png";
+    if (!fs.existsSync(maskPath)) {
+      throw new Error(`Mask file not found at path: ${maskPath}`);
+    }
+
+    // 変換した画像データでOpenAI APIを呼ぶ
+    console.log('Sending request to OpenAI API.');
+    const response = await openai.images.edit({
+      image: fs.createReadStream("public/image_edit_original.png"),
+      mask: fs.createReadStream("public/resized_mask.png"),
+      n: 1,
+      size: "256x256",
+      prompt: "A cat dressed in Santa's outfit is sitting beside a dog clad in reindeer attire."
+    });
+    console.log('Received response from OpenAI API:', response);
+
+    // 正しいプロパティへのアクセスを確認する
+    if (response && response.data) {
+      image_url = response.data[0].url;
+      console.log('Sending back the image URL:', image_url);
+      res.send(image_url);
+    } else {
+      throw new Error('Unexpected response format from OpenAI API.');
+    }
+
+  } catch (error) {
+    console.error('An error occurred:', error);
+    res.status(500).send({
+      error: 'Image generation failed.',
       details: error.message
     });
   }
